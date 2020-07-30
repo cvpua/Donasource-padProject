f63ef01eb9f9de2d0b72e03e04c6bdd24f0d4edb
Jeff Emerson Lar <jflar@up.edu.ph>
2020-07-30 09:46:44 +0800

    Fix ui

--- client/src/components/profile/user/UserProfile.js
+++ client/src/components/profile/user/UserProfile.js
@@ -46,36 +46,41 @@ class UserProfile extends Component {
     } = this.state;
 
     return (
-      <div className="mainProfile">
-        <div className="headerHolder">
-          <div className="leftHolder">
-            <div className="photoHolder"> 
-              <img className="photo" src="https://cdn57.androidauthority.net/wp-content/uploads/2020/02/eevee-pokemon-go-1200x822.jpg" alt="pic" />  
+      <div className="profile">
+        <div className="aboveProfile">
+          <div className="headerHolder">
+            <div className="leftHolder">
+              <div className="photoHolder"> 
+                <img className="photo" src="https://cdn57.androidauthority.net/wp-content/uploads/2020/02/eevee-pokemon-go-1200x822.jpg" alt="pic" />  
+              </div>
+            </div>
+            <div className="rightHolder">
+              <div className="names">
+                <div className="displayName">{name.fname + " " + name.lname} </div>
+                <div className="userName">@{username}</div>  
+              </div>
+              <div className="editHolder">
+                <button className="edit" onClick={this.editProfile}>
+                  Edit Profile
+                </button>
+                {edit ? <EditProfile /> : null}
+              </div>
+              <div className="credibility">
+                  <div className="donations"> {donations} <br/> Donations Given </div>
+                  <div className="donationsB"> {requests} <br/> Donations Requested </div>
+              </div>
             </div>
           </div>
-          <div className="rightHolder">
-            <div className="names">
-              <div className="displayName">{name.fname + " " + name.lname} </div>
-              <div className="userName">@{username}</div>  
-            </div>
-            <div className="editHolder">
-              <button className="edit" onClick={this.editProfile}>
-                Edit Profile
-              </button>
-              {edit ? <EditProfile /> : null}
-            </div>
-            <div className="credibility">
-                <div className="donations"> {donations} <br/> Donations Given </div>
-                <div className="donationsB"> {requests} <br/> Donations Requested </div>
-            </div>
+          <div className="info">
+            <div className="intel">{location}</div>
+            <div className="intel" id="mid">{email}</div>
+            <div className="intel">{contact}</div>
           </div>
+          <div className="bio">{bio}</div>
         </div>
-        <div className="info">
-          <div className="intel">{location}</div>
-          <div className="intel" id="mid">{email}</div>
-          <div className="intel">{contact}</div>
+        <div className="profileContent">
+          Dito lalagay mga donation at request na nagawa niya na
         </div>
-        <div className="bio">{bio}</div>
       </div>
     );
   }
