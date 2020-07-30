820c5bf185e45d9365c2e5b7b1102ddd18e27de6
cvpua <cvpua@up.edu.ph>
2020-07-30 17:08:16 +0800

    Fix directory structure for routes

--- routes/api/posts/posts.js
+++ routes/api/posts/posts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const posts = require('../../LocalDB/Posts');
+const posts = require('../../../LocalDB/Posts');
 
 router.get('/api/posts',(req,res) => {
     res.json(posts);
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const postsApi = require('./api/posts/posts');
+const postsApi = require('./routes/api/posts/posts');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
