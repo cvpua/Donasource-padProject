e855ddd6af06f34098db189a2ce3f3007a99b6c2
cvpua <cvpua@up.edu.ph>
2020-08-05 09:40:48 +0800

    Connect to mongo db atlas

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : "mongodb+srv://grouptwo:padthebest@donasource.8omhn.mongodb.net/<dbname>?retryWrites=true&w=majority"
+}
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+const db = require('./config/keys').mongoURI;
+
 
 const postsApi = require('./routes/api/posts');
 const usersApi = require('./routes/api/users');
+const signupApi = require('./routes/api/signup');
+
+
+mongoose.connect(db,{useNewUrlParser:true,useUnifiedTopology:true})
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
@@ -10,6 +19,7 @@ app.use(express.urlencoded({extended:false}))
 
 app.use('/', postsApi);
 app.use('/',usersApi);
+app.use('/',signupApi);
 
 
 const port = process.env.PORT || 5000;
