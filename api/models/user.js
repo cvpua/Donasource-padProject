7ffade6c84075e97d8def773e290433d5e4760ad
cvpua <cvpua@up.edu.ph>
2020-08-06 17:30:54 +0800

    Added regex in emai attribute and property

--- api/models/user.js
+++ api/models/user.js
@@ -2,13 +2,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const RequestPost = require('./requestPost');
 
+
+mongoose.set('useCreateIndex', true);
+
 const user = new Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    username : {type: String, required : true},
+    username : {
+        type: String, 
+        required : true,
+        unique : true
+    },
     password : {type : String, required : true},
     firstName : {type : String, required : true},
     lastName : {type : String, required : true},
-    email : {type: String, required : true},
+    email : {
+        type: String, 
+        required : true,
+        unique : true, 
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
     password : {type : String, required : true},
     contactNumber : {type : String, required : true},
     description : {type : String, required : true},
