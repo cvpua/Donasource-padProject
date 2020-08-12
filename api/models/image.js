7fc89a72ed6b0d93f9e0c75e9b37a0d48672eb4c
cvpua <cvpua@up.edu.ph>
2020-08-12 22:44:49 +0800

    Add image model again

---
+++ api/models/image.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const image = new Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    
+    image : {
+        imageName : {type: String},
+        url : {type: String},
+    },
+    caption : {type: String, default:null}
+})
+
+
+module.exports = mongoose.model('Image',image);
+
+
+
