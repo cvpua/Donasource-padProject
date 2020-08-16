57353f03c9f0a30bb8fb5c05716152358752645d
cvpua <cvpua@up.edu.ph>
2020-08-16 22:43:30 +0800

    Add delete post function and transfer other functions to controllers

--- api/routes/signup.js
+++ api/routes/signup.js
@@ -5,119 +5,16 @@ const brcypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 
+const Auth = require('../controllers/signup');
 const User = require('../models/user');
 const { response } = require('express');
 
 
 
-router.post('/api/signup',(req,res) => {
-   
-    User.find({email : req.body.email})
-    .exec()
-    .then( user => {
-        if(user.length >= 1){
-            return res.status(409).json({
-                message : "Email is already used"
-            })
-        }else{
-            
-            const salt = req.body.username;
-            
-            brcypt.genSalt(10,(err,salt) => {
-                brcypt.hash(req.body.password,salt,(err,hash) => {
-
-                    if(err){
-                        res.status(500).json({err});
-                        return;
-                    }else{
-                        const newUser = new User({
-                            _id: new mongoose.Types.ObjectId(),
-                            username : req.body.username,
-                            password : hash,  
-                            name : {
-                                firstName : req.body.firstName,
-                                lastName : req.body.lastName
-                            },
-                            email : req.body.email,
-                            location : req.body.location,
-                            contactNumber : req.body.contactNumber,
-                        })
-
-                        newUser.save()
-                        .then( user => {
-                            if (user){
-                                res.status(200).json({
-                                    message : "User created!"
-                                })
-                            }
-                        })
-                        .catch(error =>{
-                            const message = error.keyValue ? "Username is already used" : "Invalid email format"
-                                res.status(401).json({
-                                    error
-                                })
-                        })
-                    }
-                });
-            });
-            
-            
-        }
-    })
-}) 
+router.post('/api/signup', Auth.signup) 
 
 
 // login user
-router.post('/api/login',(req,res) => {
-    
-    User.findOne({email : req.body.email})
-    .exec()
-    .then(user => {
-        if (!user){
-            return res.status(401).json({
-                message : "Email/password is does not exist"
-            })
-        }
-
-        brcypt.compare(req.body.password,user.password, (err,result) => {
-            if (err){   
-                return res.status(401).json({
-                    message: "Email/password is does not exist"
-                  });
-            }
-            if (result){
-                const token = 
-            jwt.sign({
-                email : user.email,
-                userId : user._id, 
-                
-            },
-            process.env.JWT_KEY,
-            {
-                expiresIn : "1h"
-            })
-                return res.status(200).json({
-                    message : "Logged in",
-                    username : user.username,
-                    email : user.email,
-                    isLoggedIn : true,
-                    
-                    token : token
-                    
-                });
-            }
-        });
-    })
-    .catch(err =>{
-       
-        res.status(500).json({error : err})
-    })
-})
-
-
-router.post('/api/tester',(req,res) => {
-    console.log(req.body)
-    res.json(req.body.arr)
-})
+router.post('/api/login', Auth.login)
 
 module.exports = router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,78 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require("mongoose");
-const jwt = require('jsonwebtoken');
-
-// const multer = require('multer');
-// const upload = multer({dest : 'uploads/'});
 
 
+const UserController = require('../controllers/user');
 
-
-const User = require('../models/user');
 
 // get all user
-router.get('/api/users',(req,res) => {
-    User.find()
-    .exec()
-    .then((users) =>{
-    
-    const response =  
-    users.map(user =>{
-            return (
-                {
-                    userId: user._id,
-                    email : user.email,
-                    username : user.username,
-                    name : user.name.firstName + " " + user.name.lastName,
-                    posts : user.posts
-                }
-            )
-        })
-        res.status(200).json(response)
-    })
-})
+router.get('/api/users',UserController.getAllUsers)
 
 
 // get one user
-router.get('/api/users/:userId',(req,res) =>{
-    User.findOne({_id : req.params.userId})
-    .exec()
-    .then( user => {
-        if (!user){
-            return res.status(401).json({
-                message : "Username/password does not exist"
-            })
-        }else{
-            return res.status(200).json({
-                user
-            })
-        }
-    })
-    .catch(error => {
-        res.status(400).json({
-            message : "User not found",
-            error : error.response
-        })
-    })
-})
+router.get('/api/users/:userId',UserController.getUser)
 
 
 
 // get all post liked by a user
-router.get('/api/users/:userId/likedPosts',(req,res) => {
+router.get('/api/users/:userId/likedPosts',UserController.getLikedPosts)
 
-    User.findOne({_id : req.params.userId})
-    .populate("likedPosts")
-    .exec()
-    .then(user => {
-       
-        res.status(200).json(user.likedPosts)
-    })
-    .catch(err => {
-        res.status(400).json({message: "User not found",err})
-    })
-    
-})
 
 module.exports = router;
