04be2e37f0acb4137fa395dc9bae9ecd72302790
cvpua <cvpua@up.edu.ph>
2020-08-16 22:42:35 +0800

    Add controllers

---
+++ api/controllers/post.js
@@ -0,0 +1,346 @@
+const mongoose = require("mongoose");
+const multer = require('multer');
+const fs = require('fs-extra');
+
+const checkAuth = require('../auth/check-auth');
+
+const Post = require('../models/post');
+const User = require('../models/user');
+const Item = require('../models/item');
+const Image = require('../models/image');
+const Comment = require('../models/comment');
+const { response } = require("express");
+
+
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null, './uploads'); 
+    },
+    filename : function(req,file,cb){
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
+
+// File type checker
+const fileFilter = (req,file,cb) => {
+    if(file.mimetype === 'image/png' || file.mimetype === 'image/jpeg'){
+        cb(null,true);
+    }else{
+        cb(new Error("Wrong file format"),false);
+    }
+}
+
+const upload = multer({
+    storage, 
+    limits: {
+        fileSize: 1024 * 1024 * 3
+    },
+    fileFilter
+
+}).array('photos',3);
+
+
+exports.getAllPosts = (req,res) => {
+
+
+    Post.find()
+    .populate("likers", "username")
+    .exec()
+    .then(Posts => {
+        const response = Posts.map(post => {
+            return post
+        })
+        res.status(200).json({
+            postCount: response.length,
+            response})
+    })
+    
+
+}
+
+exports.getPost = (req,res) => {
+
+    Post.findOne({_id : req.params.postId})
+    .exec()
+    .then(post=>{
+        res.status(200).json(post)
+    })
+    .catch(err => {
+        res.status(400).json({message : "Post not found",err})
+    })
+}
+
+
+exports.makePost = (req,res) => {
+
+    fs.ensureDirSync('./uploads');
+
+    upload(req,res,function(err){
+        if(err){
+            
+            res.status(415).json({
+                message:"Error in uploading images in the uploads folder (only 3 images are allowed per post)",
+                response: err
+            });
+        }
+        else{
+
+            let title = req.body.title.replace(/\s/g, ''); //remove spaces on strings
+
+            const dir = 'assets/' + req.body.author + '/posts/' + title + '_' + Date.now() + '/images/';
+
+            let imageArray = null;
+            if(req.files && req.files.length > 1){
+                
+                     imageArray = req.files.map(file =>{
+                       
+                         image = new Image({
+                            _id: new mongoose.Types.ObjectId(),
+                            total: req.body.total,
+                            image: {
+                                imageName : file.filename, 
+                                url: 'http://localhost:5000/'+dir+file.filename 
+                            }
+                    });
+                        return(image)
+                })
+                
+            }
+            
+           
+            const post = new Post({
+                _id: new mongoose.Types.ObjectId(),
+                avatar : req.body.avatar,
+                title : req.body.title,
+                author : req.body.author,
+                type : req.body.type,
+                status : req.body.status,
+                description : req.body.description,
+                items : [],
+                location : req.body.location,
+                tags : req.body.tags,
+                datePosted : Date.now(),
+                deadline: Date.now(),
+                photos : imageArray,
+                comments : []
+            })   
+            
+            // There is images and needs to create folders and move images
+            if(req.files.length > 0){ 
+              
+                fs.move('./uploads',dir,(error)=>{
+                
+                    if (error){
+                
+                        res.status(500).json({
+                            message: "Error in moving images to the assets folder",
+                            error
+                            
+                    });
+                    }else{
+                        User.findOne({email : req.userData.email}).exec()
+                        .then(user => {
+                            user.posts.push(post);
+
+                            if(post.type === "request"){
+                                user.requestCount = user.requestCount + 1 
+                            }else{
+                                user.donationCount = user.donationCount + 1;
+                            }
+                            user.postCount = user.postCount + 1;
+                            user.save()
+                            .then(
+                                post.save()
+                                .then(
+                                    res.status(200).json({
+                                        message:"Post created!",
+                                        post
+                                    })
+                                )
+                                .catch(err => {
+                                    console.log(err)
+                                    res.json(err)
+                                })
+                            )
+                            .catch(err => {
+                                console.log(err)
+                                res.json(err)
+                            })
+                        })
+                        .catch( err => {
+                            
+                            console.log(err)
+                            res.status(500).json("Error occured")
+                        })                   
+                    }
+                    
+                })
+            }
+            // There are no images, no need to make folders
+            else{
+                User.findOne({email : req.userData.email}).exec()
+                        .then(user => {
+                            user.posts.push(post);
+                            post.type === "request" ? user.requestCount = user.requestCount + 1 : user.donationCount = user.donationCount + 1
+                            user.postCount = user.postCount + 1;
+                            user.save()
+                            .then(
+                                post.save()
+                                .then(
+                                    res.status(200).json({
+                                        message:"Post created!",
+                                        post
+                                    })
+                                )
+                                .catch(err => {
+                                    console.log(err)
+                                    res.json(err)
+                                })
+                            )
+                            .catch(err => {
+                                console.log(err)
+                                res.json(err)
+                            })
+                        })
+                        .catch( err => {
+                            
+                            console.log(err)
+                            res.status(500).json("Error occured")
+                        })                   
+            }
+        }
+    });    
+    
+}
+
+exports.makeComment = (req,res) => {
+    
+    Post.findOne({_id : req.params.postId})
+    .exec()
+    .then(
+        post => {
+            const comment = new Comment({
+                _id: new mongoose.Types.ObjectId(),
+                user: {
+                    username : req.body.username,
+                    avatar : req.body.avatar,
+                },
+                content : req.body.content,
+                postId : post._id
+            })
+            let comments = post.comments;
+            comments.push(comment)
+            post.comments = comments;
+            post.save()
+            .then( response => {
+                res.status(200).json({message : "Comment created!", post})
+            })
+            .catch(err =>{
+                res.status(500).json({message : "Comment not created",err})
+            })
+    })
+    .catch(err => {
+        res.status(400).json({message : "Post not found!",err})
+    })
+
+    
+}
+
+exports.likePost = (req,res) => {
+    
+    User.findOne({_id : req.body.userId})
+    .exec(function(err,foundUser){
+        if(!err && foundUser){
+            const alreadyLiked = foundUser.likedPosts.includes(req.params.postId);
+            if(alreadyLiked){
+                Post.findOne({_id : req.params.postId})
+                .exec()
+                .then(
+                     post => {
+                        let likes = post.likes;
+                        likes = likes - 1;
+                        post.likes = likes;
+                        post.likers = post.likers.filter(userId => String(userId) !== req.body.userId)
+                        post.save()
+                        .then( response => {
+                            foundUser.likedPosts = foundUser.likedPosts.filter(postId => String(postId) !== req.params.postId)
+                            foundUser.likedPostsCount = foundUser.likedPostsCount - 1;
+                            foundUser.save()
+                                .then (response => {
+                                     res.status(200).json({message : "Post unliked!", post})
+                                })     
+                            
+                        })
+                        .catch(err =>{
+                            res.status(500).json({message : "Post not unliked!",err})
+                        })
+                    })
+                    .catch(err => {
+                        res.status(400).json({message : "Post not found!",err})
+                    })    
+            }else{
+                Post.findOne({_id : req.params.postId})
+                .exec()
+                .then(
+                     post => {
+                        let likes = post.likes;
+                        likes = likes + 1;
+                        post.likes = likes;
+                        post.likers.push(req.body.userId);
+                        post.save()
+                        .then( response => {
+                            foundUser.likedPosts.push(post);
+                            foundUser.likedPostsCount = foundUser.likedPostsCount + 1;
+                            foundUser.save()
+                                .then (response => {
+                                     res.status(200).json({message : "Post liked!", post})
+                                })     
+                            
+                        })
+                        .catch(err =>{
+                            res.status(500).json({message : "Post not liked!",err})
+                        })
+                    })
+                    .catch(err => {
+                        res.status(400).json({message : "Post not found!",err})
+                    })
+            }
+
+            
+        }else{
+            res.status(500).json({message : "User not found", err})
+        }
+    })
+    
+}
+
+
+exports.deletePost = (req,res) => {
+    Post.deleteOne({_id : req.params.postId})
+    .exec()
+    .then(post => {
+        User.findOne({username : req.body.username})
+        .exec()
+        .then(user =>{
+            user.posts = user.posts.filter(post => String(post._id) !== req.params.postId)
+            user.save()
+            .then(response => {
+                res.status(200).json({message : "Post deleted!",post})
+            })
+            .catch( err =>
+                console.log(err)
+            )
+        })
+        .catch( err =>
+            console.log(err)
+        )
+
+
+        
+    })
+    .catch(err => {
+        res.status(500).json({message: "Unable to delete post",
+        err})
+        
+    })
+}
---
+++ api/controllers/signup.js
@@ -0,0 +1,111 @@
+const mongoose = require("mongoose");
+const brcypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+const User = require('../models/user');
+
+
+exports.signup = (req,res) => {
+   
+    User.find({email : req.body.email})
+    .exec()
+    .then( user => {
+        if(user.length >= 1){
+            return res.status(409).json({
+                message : "Email is already used"
+            })
+        }else{
+            
+            const salt = req.body.username;
+            
+            brcypt.genSalt(10,(err,salt) => {
+                brcypt.hash(req.body.password,salt,(err,hash) => {
+
+                    if(err){
+                        res.status(500).json({err});
+                        return;
+                    }else{
+                        const newUser = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            username : req.body.username,
+                            password : hash,  
+                            name : {
+                                firstName : req.body.firstName,
+                                lastName : req.body.lastName
+                            },
+                            email : req.body.email,
+                            location : req.body.location,
+                            contactNumber : req.body.contactNumber,
+                        })
+
+                        newUser.save()
+                        .then( user => {
+                            if (user){
+                                res.status(200).json({
+                                    message : "User created!"
+                                })
+                            }
+                        })
+                        .catch(error =>{
+                            const message = error.keyValue ? "Username is already used" : "Invalid email format"
+                                res.status(401).json({
+                                    error
+                                })
+                        })
+                    }
+                });
+            });
+            
+            
+        }
+    })
+}
+
+exports.login = (req,res) => {
+    
+    User.findOne({email : req.body.email})
+    .exec()
+    .then(user => {
+        if (!user){
+            return res.status(401).json({
+                message : "Email/password is does not exist"
+            })
+        }
+
+        brcypt.compare(req.body.password,user.password, (err,result) => {
+            if (err){   
+                return res.status(401).json({
+                    message: "Email/password is does not exist"
+                  });
+            }
+            if (result){
+                const token = 
+            jwt.sign({
+                email : user.email,
+                userId : user._id, 
+                
+            },
+            process.env.JWT_KEY,
+            {
+                expiresIn : "1h"
+            })
+                return res.status(200).json({
+                    message : "Logged in",
+                    username : user.username,
+                    email : user.email,
+                    isLoggedIn : true,
+                    
+                    token : token
+                    
+                });
+            }
+        });
+    })
+    .catch(err =>{
+       
+        res.status(500).json({error : err})
+    })
+}
+
+
