4fcc51c0662f6daed44c81ff72d05ac213fb050d
Jeff Emerson Lar <jflar@up.edu.ph>
2020-08-25 17:15:24 +0800

    Integrate search filter

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -5,30 +5,11 @@ const INITIAL_STATE = {
     category: '',
 };
 
-const sample = [
-    {
-        id: 1,
-        title: "I need alcohol pls guys",
-        author: "Marco Mirandilla",
-        tags: ['Educational', 'Food'],
-    },
-    {
-        id: 2,
-        title: "Penge barya kyah",
-        author: "Jep Lar",
-        tags: ['Drinks', 'Food'],
-    },
-    {
-        id: 3,
-        title: "For online classes",
-        author: "CV Pua",
-        tags: ['Food','Shelter'],
-    },
-]
 
 class Search extends Component {
     state = INITIAL_STATE;
 
+    
     updateSearch = (event) => {
         const { target } = event;
         this.setState({ search: target.value });
@@ -36,26 +17,36 @@ class Search extends Component {
     
     handleFormSubmit = (event) => { 
         const { category } = this.state;
+        const { setFilteredPost, posts } = this.props;
         event.preventDefault();
         
         this.setState(INITIAL_STATE);
-        let filteredPost = sample.filter(
+        let filteredPost = posts.filter(
             (post) => {
-                console.log(category)
                 if (category === "title") {
                     return post.title.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
                 } else if (category === "author") {
                     return post.author.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
                 } else if (category === "tags"){
-                    post.tags.filter((item) => {
-                        if (item.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1) {
-                            console.log(post)
+                    let indicator = post.tags.map((item) => {
+                        if (item.toLowerCase() === this.state.search.toLowerCase()){
+                            return true;
                         }
                     })
+                    if (indicator.includes(true) == true) {
+                        return true;
+                    } else {
+                        return false;
+                    }
                 }
             }
-        );
-    }
+            );
+            setFilteredPost(filteredPost)
+            // filteredPost.map((post) => {
+            //     console.log(post)
+            //    
+            // })
+        }
         
     changeCategory = (event) => {
         const { target } = event;
@@ -83,11 +74,11 @@ class Search extends Component {
                         />
                     <br />
                     <div onChange={this.changeCategory}>
-                        <input type="radio" id="title" name="categories" value="title" checked={this.state.category === 'title'} />
+                        <input type="radio" id="title" name="categories" value="title" checked={category === 'title'} />
                         <label for="title" />Title <br />
-                        <input type="radio" id="author" name="categories" value="author" checked={this.state.category === 'author'}/>
+                        <input type="radio" id="author" name="categories" value="author" checked={category === 'author'}/>
                         <label for="author" />Author<br />
-                        <input type="radio" id="tags" name="categories" value="tags" checked={this.state.category === 'tags'}/>
+                        <input type="radio" id="tags" name="categories" value="tags" checked={category === 'tags'}/>
                         <label for="tags" />Tags <br />
                     </div>
                     <button style={{ backgroundColor: "lightblue" }}> Search </button>
