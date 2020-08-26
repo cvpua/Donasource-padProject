40a3d6744ee4e6945fe29e9a5529b74b88837c77
cvpua <cvpua@up.edu.ph>
2020-08-26 22:33:16 +0800

    Add attributes to models for easy manipulation of data

--- api/models/comment.js
+++ api/models/comment.js
@@ -6,6 +6,7 @@ const comment = new Schema({
     
     user : {
         type : {type : String},
+        name : {type: String, required : true},
         username : {type: String, required : true},
         avatar : {type: String,  default: null},
     },
