c25bc3de0a7d98da696c6d6ca8742c150d328a48
cvpua <cvpua@up.edu.ph>
2020-08-28 23:26:06 +0800

    Add logout function

--- api/auth/check-auth.js
+++ api/auth/check-auth.js
@@ -5,28 +5,32 @@ const { response } = require('express');
 
 module.exports = (req,res,next) => {
     
-    
     const token = req.headers.authorization.split(" ")[1]
     blacklist.findOne({token : token})
     .exec()
-    .then(token => {
-        return res.status(401).json({
-            message : "Please login first"
-        })
-    })
-    .catch(err => {
-        try{
-    
-        
-            const decoded = jwt.verify(token, process.env.JWT_KEY);
-            req.userData = decoded 
-            next()
+    .then(foundToken => {
+
+        if(!foundToken){
+                   
+            try{
+                const decoded = jwt.verify(token, process.env.JWT_KEY);
+                req.userData = decoded 
+                next()
+            }
+            catch(err){
+                return res.status(401).json({
+                    message : "Please login first"
+                })
+            }
         }
-        catch(err){
+        else{
             return res.status(401).json({
                 message : "Please login first"
             })
         }
+    })
+    .catch(err => {
+        console.log(err)
     })
 
     
