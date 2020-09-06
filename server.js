87f3a1b6d9c5a51b277ce2e700c8162697aecde1
rcmirandilla <rcmirandilla@up.edu.ph>
2020-09-06 13:26:50 +0800

    Add changes to server.js

--- server.js
+++ server.js
@@ -27,10 +27,10 @@ app.get("*", (req, res) => {
 
   if (protected.includes(path)) {
     // Return the actual file
-    res.sendFile(`./build/${path}`);
+    res.sendFile(`Donasource/build/${path}`);
   } else {
     // Otherwise, redirect to /build/index.html
-    res.sendFile(`./build/index.html`);
+    res.sendFile(`Donasource/build/index.html`);
   }
 });
 
